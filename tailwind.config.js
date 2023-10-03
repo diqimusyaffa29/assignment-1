84283990461cce91870aff6ed2994ded8e2d49b8
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2023-10-04 00:19:49 +0800

    Bismillah bisa, Progress Mobile View

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppin: ['Poppins'],
+        raleway: ['Raleway'],
+      }
+    },
+  },
+  plugins: [],
+}
+
